4af64b4a53afb82a1fcdbf0f4be149bb4874313c
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-24 09:29:26 -0600

    solved challenge

--- CodeChallenge-DecodeString.js
+++ CodeChallenge-DecodeString.js
@@ -1,1 +1,17 @@
+const stringDecoder = (string) => {
+  let splitString = string.split("");
+  let decoded = " ";
 
+  for (let i = 0; i < splitString.length; i++) {
+    if (!isNaN(+splitString[i])) {
+      i += +splitString[i];
+    } else {
+      decoded += splitString[i];
+    }
+  }
+  return console.log(decoded);
+};
+
+stringDecoder("0y4akjfe0s");
+stringDecoder("0h2xce5ngbrdy");
+stringDecoder("2bna0p1mp2osl0e");
