98abdd57cd3b81002b919ff4c64f3ee1198fa9fb
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-24 10:20:07 -0600

    alternate solution

--- CodeChallenge-DecodeString.js
+++ CodeChallenge-DecodeString.js
@@ -15,3 +15,28 @@ const stringDecoder = (string) => {
 stringDecoder("0y4akjfe0s");
 stringDecoder("0h2xce5ngbrdy");
 stringDecoder("2bna0p1mp2osl0e");
+
+// alternate solution:
+
+// function decoder(str) {
+//   const shiftAmt = +str[0]
+//   const toDecode = str.slice(1)
+
+//   const strCodesArr = [];
+
+//   let returnStr = '';
+
+//   for (let i = 0; i < toDecode.length; i++) {
+//       strCodesArr.push(toDecode.charCodeAt(i))
+//   }
+
+//   for (let i = 0; i < strCodesArr.length; i++) {
+//       const updatedCode = strCodesArr[i] + shiftAmt;
+
+//       returnStr += String.fromCharCode(updatedCode)
+//   }
+
+//   return returnStr;
+// }
+
+// decoder('2fcjjm')
